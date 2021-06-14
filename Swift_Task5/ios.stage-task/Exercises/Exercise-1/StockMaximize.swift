1e48ad6c49f599afe2fc2ab9036124ffae3e6b60
dnlrknts <74173552+dnlrknts@users.noreply.github.com>
2021-06-14 23:29:23 +0300

    feat: add task5

---
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+class StockMaximize {
+    
+    func countProfit(prices: [Int]) -> Int {
+        if prices.count == 0 || prices.count == 1 {
+            return 0
+        }
+        
+        var profit = 0
+        var dayWhenCanSell = prices
+        
+        for i in 0..<prices.count - 1 {
+            dayWhenCanSell.remove(at: 0)
+            guard let maxPrice = dayWhenCanSell.sorted().last else {
+                return 0
+            }
+            if prices[i] < maxPrice{
+                profit += maxPrice - prices[i]
+            }
+        }
+        return profit
+    }
+}
---
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+class MessageDecryptor: NSObject {
+    
+    func decryptMessage(_ message: String) -> String {
+        var counts = [String]()
+        var tempCount = ""
+        var repeatString = [String]()
+        var result = ""
+   
+        for char in message {
+            if char.isNumber {
+                tempCount = "\(tempCount)\(char)"
+            } else if char == "[" {
+                if tempCount == "" {
+                    counts.append("1")
+                } else {
+                    counts.append(tempCount)
+                }
+                tempCount = ""
+                repeatString.append(result)
+                result = ""
+            } else if char == "]" {
+                var tempString = ""
+                if let counts = Int(counts.popLast() ?? "1") {
+                    for _ in 0..<counts {
+                        tempString += result
+                    }
+                }
+                result = repeatString.popLast()! + tempString
+            } else if char.isLetter {
+                result += String(char) 
+            }
+        }
+        
+        return result
+    }
+}
